a35821b3a906d9666fda5bef4dbac0eabec7c9c2
Oluwarotimi Adewale <merlinwaysgaming@gmail.com>
2022-10-30 04:19:53 +0100

    chore:Added routing function

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='absolute bottom-0 h-[40px] bg-black/90 text-white text-2xl w-full flex items-center justify-end px-2'>
+    <div className='z-[80] absolute bottom-0 h-[40px] bg-black/90 text-white text-2xl w-full flex items-center justify-end px-2'>
       <h1>Total Posts: 1</h1> 
     </div>
   )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='w-full h-[100px] flex flex-col p-4 flex-grow-0 bg-black/80 text-white justify-center'>
         <div>
-            <h1 className='text-4xl font-bold tracking-wider'>Simple Blog</h1>
+            <h1 className='text-4xl font-bold tracking-wider'>Simple CRUD </h1>
         </div>
     </div>
   )
