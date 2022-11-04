01d9447ce5786a24a75323080421b4df4940b26b
Oluwarotimi Adewale <merlinwaysgaming@gmail.com>
2022-11-04 14:45:59 +0100

    chore: fixed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useStore from '../store/useStore'
+
+
+
+
 
 const Footer = () => {
+
+  const post = useStore((state) => state.post)
+  const fetchPost = useStore((state) => state.fetchPost)
+
+  useEffect(() => {
+    fetchPost()
+    
+  }, [])
+
   return (
     <div className='z-[80] absolute bottom-0 h-[40px] bg-black/90 text-white text-2xl w-full flex items-center justify-end px-2'>
-      <h1>Total Posts: 1</h1> 
+      <h1>Total Posts: {post.length}</h1> 
     </div>
   )
 }
--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -3,5 +3,19 @@ import Post from "../../../data/Post"
 
 
 export default function handler(req,res) {
-    res.status(200).json(Post)
+    if (req.method === 'GET') {
+        res.status(200).json(Post)
+    }
+    else if(req.method === 'POST') {
+
+        const comment = req.body.comment
+        const title = req.body.title
+        const id = Date.now()
+        
+        const newPost = {comment,title,id}
+        console.log(Post)
+        
+        Post.push(newPost)
+        req.status(200).json(Post)
+    }
 }
